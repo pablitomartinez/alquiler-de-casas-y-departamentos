f7942b0efc6c4065261481ea086148d33ef42b53
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-06-25 17:24:36 -0300

    se agrego dark-mode segun preferencias del usuario

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,29 @@ document.addEventListener('DOMContentLoaded', function(){
     darkMode();
 })
 
+
 function darkMode(){
+    const prefiereDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // console.log(prefiereDarkMode.matches);
+
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode')
+    }
+    else {
+        document.body.classList.remove("dark-mode");
+    }
+
+    //hacer que la preferencia se actualice
+    prefiereDarkMode.addEventListener('change', function(){
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode')
+        }
+        else{
+            document.body.classList.remove("dark-mode");
+        }
+    })
+
     const botonDarkMode = document.querySelector(".dark-mode-boton");
     botonDarkMode.addEventListener('click', function(){
         document.body.classList.toggle('dark-mode')
